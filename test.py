f66eb9b2262ff8ecb2e6ddc575b1c9b751460c20
pbarnickel <philippbarnickel@gmail.com>
2018-06-09 00:47:35 +0200

    python test script

--- test.py
+++ test.py
@@ -1,1 +1,17 @@
-print('Test')
+import random
+
+random.seed()
+
+a = random.randint(1, 10)
+b = random.randint(1, 10)
+
+c = a + b
+
+print(a, ' + ', b, ' = ')
+
+i = int(input())
+
+if i != c:
+    print('The result is not ', i, '. It is ', c, '.')
+else:
+    print('You are right! The result is ', i, '.')
