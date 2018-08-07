03e1d6d9573cd1733f5e876d138b9f58859a6feb
pbarnickel <philippbarnickel@gmail.com>
2018-08-07 11:57:54 +0200

    simple test

--- test.py
+++ test.py
@@ -1,17 +1,1 @@
-import random
-
-random.seed()
-
-a = random.randint(1, 10)
-b = random.randint(1, 10)
-
-c = a + b
-
-print(a, ' + ', b, ' = ')
-
-i = int(input())
-
-if i != c:
-    print('The result is not ', i, '. It is ', c, '.')
-else:
-    print('You are right! The result is ', i, '.')
+# simple test
